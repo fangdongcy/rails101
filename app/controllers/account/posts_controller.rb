36168d41e1050d437fbd6c817e4ae710a1ca6d46
bigmao <zjufd2013@gmail.com>
2017-07-27 14:56:33 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,25 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = current_user.posts.find(params[:i])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:i])
+    @post.destroy
+    flash[:alert] = "post deleted"
+    redirect_to posts_path
+  end
+
+
 end
